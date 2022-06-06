90f22d6a93120d615a3eb1cd64b0d4e3c9dacbae
rajendra7117 <rajendrabvm0007@gmail.com>
2022-06-06 14:02:12 +0530

    assesment-task

--- src/components/Main/Menu.js
+++ src/components/Main/Menu.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, memo } from "react";
 import "./Menu.css";
-
-
+import { debounce } from "lodash";
+import { useCallback } from "react";
 import ItemContainer from "./ItemContainer";
 const Menu = () => {
   const [items, setItems] = useState([]);
-  const [list, setList] = useState([])
+  const [list, setList] = useState([]);
+  const [count, setCount] = useState(0);
 
- 
   useEffect(() => {
     fetch("https://run.mocky.io/v3/9d71cb03-a9f9-4d70-bae2-9d3adaa1cfe7")
       .then((res) => {
@@ -15,21 +15,53 @@ const Menu = () => {
       })
       .then((data) => {
         setItems(data);
-        setList([data.splice(0, 20)])
-       
+        let arr = [];
+        for (let i = 0; i <= 10; i++) {
+          arr.push(data[i])
+        }
+        setList(arr)
+        setCount(10);
       });
   }, []);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, true);
+  });
 
+  const debounceEvent = useCallback(
+    debounce((e) => {
+      if (
+        e.target.scrollTop + window.innerHeight + 1 >=
+        e.target.scrollHeight
+      ) {
+        setCount((prev) => prev + 10);
+      }
+    }, 1000),
+    []
+  );
+  const handleScroll = (e) => {
+    debounceEvent(e);
+  };
+
+  useEffect(() => {
+    let arr = [];
+        if(count>10 && count<items.length-11){
+          for (let i = count + 1; i < count + 10; i++) {
+                arr.push(items[i]);
+              }
+              setList((prev) => [...prev, ...arr]);
+        }
+    
+  }, [count]);
 
   return (
     <div className="items-container">
       <div className="menu">
-        {items.length > 0 ? (
-          <ItemContainer items={items} />
+        {list.length > 0 ? (
+          <ItemContainer items={list} />
         ) : (
           <div className="loader">
-         <span>Loading...</span>
+            <span>Loading...</span>
           </div>
         )}
       </div>
