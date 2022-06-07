6df5652a6dcc7e2b9c90b8f51741f51fcd643bad
rajendra7117 <rajendrabvm0007@gmail.com>
2022-06-07 22:29:35 +0530

    assesment-task

--- src/components/Main/Menu.js
+++ src/components/Main/Menu.js
@@ -3,10 +3,12 @@ import "./Menu.css";
 import { debounce } from "lodash";
 import { useCallback } from "react";
 import ItemContainer from "./ItemContainer";
+import Loader from "../Modal/Loader";
 const Menu = () => {
   const [items, setItems] = useState([]);
   const [list, setList] = useState([]);
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     fetch("https://run.mocky.io/v3/9d71cb03-a9f9-4d70-bae2-9d3adaa1cfe7")
@@ -16,11 +18,11 @@ const Menu = () => {
       .then((data) => {
         setItems(data);
         let arr = [];
-        for (let i = 0; i <= 10; i++) {
+        for (let i = 0; i < 12; i++) {
           arr.push(data[i])
         }
         setList(arr)
-        setCount(10);
+        setCount(12);
       });
   }, []);
 
@@ -30,25 +32,28 @@ const Menu = () => {
 
   const debounceEvent = useCallback(
     debounce((e) => {
+      
       if (
         e.target.scrollTop + window.innerHeight + 1 >=
         e.target.scrollHeight
       ) {
-        setCount((prev) => prev + 10);
+        setCount((prev) => prev + 11);
       }
     }, 1000),
     []
   );
   const handleScroll = (e) => {
+
     debounceEvent(e);
   };
 
   useEffect(() => {
     let arr = [];
-        if(count>10 && count<items.length-11){
-          for (let i = count + 1; i < count + 10; i++) {
+        if(count>12 && count<items.length-12){
+          for (let i = count+1; i < count + 12; i++) {
                 arr.push(items[i]);
               }
+            
               setList((prev) => [...prev, ...arr]);
         }
     
@@ -60,11 +65,10 @@ const Menu = () => {
         {list.length > 0 ? (
           <ItemContainer items={list} />
         ) : (
-          <div className="loader">
-            <span>Loading...</span>
-          </div>
+          <Loader />
         )}
       </div>
+
     </div>
   );
 };
